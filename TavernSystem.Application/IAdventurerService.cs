fd539338786e3c68460dcafee9ab26d3518522ae
wojteksuj <s31087@pjwstk.edu.pl>
2025-05-09 11:40:49 +0200

    Last commit

--- TavernSystem.Application/IAdventurerService.cs
+++ TavernSystem.Application/IAdventurerService.cs
@@ -6,4 +6,5 @@ public interface IAdventurerService
 {
     public IEnumerable<AdventurerIdNickname> GetAllAdventurers();
     public AdventurerPerson? GetAdventurerById(int id);
+    public bool addAdventurer(int id, string nickname, int idRace, int idExperience, int idPerson);
 }
